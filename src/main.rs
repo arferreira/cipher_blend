86a25852a8410b00b56983a3d932ae62f03c21f0
Antonio Souza <arfs.antonio@gmail.com>
2023-12-12 21:37:14 -0500

    chore: add how to use on main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use cipher_blend::{decrypt, encrypt};
+
 fn main() {
-    println!("Cipher Blend");
+    let original_message = "Hello, Rust!";
+    let shift = 3;
+
+    // Encrypt
+    let encrypted_message = encrypt(original_message, shift);
+    println!("Encrypted: {}", encrypted_message);
+
+    // Decrypt
+    let decrypted_message = decrypt(&encrypted_message, shift);
+    println!("Decrypted: {}", decrypted_message);
 }
